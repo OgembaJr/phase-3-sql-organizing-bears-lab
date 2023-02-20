835f6e2312921f9276536b422aca8552378eac1e
OgembaJr <emmanuel.ogemba@student.moringaschool.com>
2023-02-20 03:28:52 +0300

    Done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 12, M, 'White', 'hottempered', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 6, M, 'White', 'hottempered', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 3, F, 'White', 'shorttempered', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergent Brown', 9, M, 'White', 'hottempered', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 7, F, 'White', 'hottempered', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 5, M, 'White', 'Calm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 14, F, 'White', 'hottempered', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL , 8, M, 'White', 'aggressive', 1);
